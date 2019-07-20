53faa4d7062dd89cdd4336faacd9d1d79b819d4f
mayhemphone <mayhemphone@gmail.com>
2019-07-19 18:12:26 -0700

    adding database structure

--- seedDatabase.js
+++ seedDatabase.js
@@ -1,1 +1,27 @@
 // Use the JSON file to seed the database
+
+let twitterData = require('./twitters2.json');
+
+// console.log(twitterData)
+
+// let stuff = twitterData.map((index, record)=>{
+// 	console.log(record)
+// })
+
+// console.log(twitterData['Martha McSally'])
+
+
+
+
+
+function entries() {
+	for (let [key, value] of Object.entries(twitterData)){
+		console.log(`${key}: ${value}`)
+
+		// TODO: write new entry to database
+			// name: key
+			// twitter: value
+	}
+
+}
+
